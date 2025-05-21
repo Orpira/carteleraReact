eb0c1600e7affe1270ddff2524909796f0594c9a
carolina <analancommty2021@gmail.com>
2025-05-21 11:06:59 +0200

    correccion signin

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -13,12 +13,6 @@ const Form = ({ onSubmit }) => {
       className="bg-white rounded-xl shadow-md p-6 w-full max-w-md mx-auto flex flex-col gap-4"
     >
       <input
-        name="nombre"
-        placeholder="Nombre"
-        className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-        required
-      />
-      <input
         name="email"
         type="email"
         placeholder="Email"
