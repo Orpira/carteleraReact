826b65f1a95f082b8db9b91124b23c44f9b9e6bc
Orpira <orpira@gmail.com>
2025-05-23 09:38:27 +0200

    importacion componente en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import Profile from "./components/Profile/Profile.jsx"; // Asegúrate de tener este componente
+import Profile from "../index.js"; // Asegúrate de tener este componente
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
